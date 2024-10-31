ef45ec39dfbbda54a5bf19ee4f92bf9f2ffe1e48
Michaeladsl <89179287+Michaeladsl@users.noreply.github.com>
2024-10-31 14:47:41 -0400

    Create setup.py
    setup.py for pipx

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_project_name',
+    version='0.1.0',
+    packages=find_packages(), 
+    install_requires=[
+        'Flask',
+        'pyte',
+        'tqdm',
+        'asciinema',
+    ],  
+    entry_points={
+        'console_scripts': [
+            'edit=edit:main',         
+            'patronus=patronus:main', 
+            'redact=redact:main',     
+            'server=server:main',    
+            'split=split:main',     
+        ],
+    },
+)
