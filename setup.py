b5e16ee6bf07f2780fd99f5b57d1b733d5114240
Michaeladsl <89179287+Michaeladsl@users.noreply.github.com>
2024-10-31 15:02:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name='Patronus',
+    name='patronus',
     version='0.1.0',
-    packages=find_packages(), 
+    py_modules=['edit', 'patronus', 'redact', 'server', 'split'],  # List of scripts in the root directory
     install_requires=[
         'Flask',
         'pyte',
         'tqdm',
         'asciinema',
-    ],  
+    ],
     entry_points={
         'console_scripts': [
-            'edit=edit:main',         
-            'patronus=patronus:main', 
-            'redact=redact:main',     
-            'server=server:main',    
-            'split=split:main',     
+            'edit=edit:main',
+            'patronus=patronus:main',
+            'redact=redact:main',
+            'server=server:main',
+            'split=split:main',
         ],
     },
 )
