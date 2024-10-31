6a4154416978d382b164d54db0dde3665fd81996
Michaeladsl <89179287+Michaeladsl@users.noreply.github.com>
2024-10-31 14:53:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your_project_name',
+    name='patronus',
     version='0.1.0',
     packages=find_packages(), 
     install_requires=[
